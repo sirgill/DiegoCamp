e6fa199f2d398cbd7c2bef19dc7ad4e8b45e7e13
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-06-27 14:15:03 -0400

    Changed port settings for deployment

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ var express = require('express');
 var app = express();
 app.use(express.static("public"));
 
+var port = process.env.PORT || 8000
 
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
+ server.listen(port, function () {
    console.log("Example app listening at http://%s:%s", host, port)
 })
