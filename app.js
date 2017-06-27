13262f9d39c36518f10044493426731324d3e30a
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-06-27 14:37:08 -0400

    Localhost changed for demo

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.static("public"));
 
-var port = process.env.PORT || 8000
 
-server.listen(port, function() {
-    console.log("App is running on port " + port);
-});
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
