42aeae1f3e3eacf259122d9456d54d7ce6942f74
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-06-27 13:53:36 -0400

    Added local server

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ var app = express();
 app.use(express.static("public"));
 
 
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
