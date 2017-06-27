f1b751489bee74b90d1df47abf67183eedf380c4
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-06-27 14:24:06 -0400

    syntax fix

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ app.use(express.static("public"));
 
 var port = process.env.PORT || 8000
 
- server.listen(port, function () {
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
